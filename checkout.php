87b57c4b4b9aa6eb6d048640861e258eddd4ea55
sitiulandasurofah <68583875+sitiulandasurofah@users.noreply.github.com>
2020-07-21 15:07:09 +0700

    Create checkout.php

---
+++ checkout.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rmah sakit</title>
+	<link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+	<html><head><title>Form Screen Center</title>
+		<div class="img">
+			<img src="rs2.jpg" height="670px" width="1350px">
+		</div>
+		<style>
+		.tengah{
+			position: absolute;
+			margin-top: -100px;
+			margin-left: -200px;left: 50%;top: 50%;
+			width: 400px;
+			background-color:grey;
+		}
+		ul{
+			margin-left:200px;
+		}
+		</style>
+	</head>
+	<body>
+	<div align="center" class="tengah">
+		<p align="center">
+			<font face="verdana" size="10" color="black">Semoga Cepat Sembuh</font>
+			</form>
+</body>
+</html>
